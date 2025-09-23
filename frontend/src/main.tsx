8e7cc89aaf5de6c4d79b0b5bee53563d2d32f645
Ali Farhani <alifarhani7727315@gmail.com>
2025-09-23 18:42:42 +0330

    Delete frontend directory

--- frontend/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import BCFrontendStarter from "./App";
-
-createRoot(document.getElementById("root")!).render(<BCFrontendStarter />);
